cb1f688a380e55603f7c725db84073fa61bba4b7
Hamerson Torres <nosremah@gmail.com>
2026-05-01 09:55:54 -0500

    Mis nuevos cambios

--- prueba-funcion-simbolica.py
+++ prueba-funcion-simbolica.py
@@ -3,7 +3,7 @@ import math
 
 #Funcion que recibe como parámetro una función lambda y retorna
 # una función simbolica con la misma estructura lógica. 
-def funcion(f_lambda):
+def f_derivada(f_lambda):
     x=sp.symbols('x')
     return sp.diff(f_lambda(x))
 
